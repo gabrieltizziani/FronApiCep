105706c4f122cccd715a4a54d76ae3d971171b7b
Gabriel de Melo Tizziani <126525209+gabrieltizziani@users.noreply.github.com>
2024-09-03 13:26:31 -0300

    Finalizando

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-document.getElementById('formEndereco').addEventListener('submit', async (event) =>{
+document.getElementById('formEndereco').addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const rua = document.getElementById('rua').value;
     const cidade = document.getElementById('cidade').value;
     const estado = document.getElementById('estado').value;
     const cep = document.getElementById('cep').value;
+    const logradouro = document.getElementById('logradouro').value;
+    const numero = document.getElementById('numero').value;
+    const complemento = document.getElementById('complemento').value;
+    const bairro = document.getElementById('bairro').value;
+    const municipioIBGE = document.getElementById('municipioIBGE').value;
 
     const addressData = {
-        rua,
-        cidade,
-        estado,
-        cep
+        Cidade: cidade,
+        Estado: estado,
+        Cep: cep,
+        Logradouro: logradouro,
+        Numero: numero,
+        Complemento: complemento,
+        Bairro: bairro,
+        MunicipioIBGE: municipioIBGE
     };
 
     try {
-        const response = await fetch ('http://localhost:3000/api/enderecos',{
+        const response = await fetch('http://localhost:3000/api/enderecos/', {
             method: 'POST',
             headers: {
-                'Content-Type' : 'aplication/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(addressData)
         });
 
-        const
+        const result = await response.json();
+
+        if (response.ok) {
+            document.getElementById('message').innerText = 'Endereço enviado com sucesso!';
+            document.getElementById('formEndereco').reset();
+        } else {
+            document.getElementById('message').innerText = `Erro: ${result.message}`;
+        }
+    } catch (error) {
+        document.getElementById('message').innerText = 'Erro na comunicação com o servidor.';
     }
-})
+});
